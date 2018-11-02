3e8a471d26f67e6a377032295f5d120cdc159157
__sp__ <shyuep@gmail.com>
2018-11-02 13:40:32 -0700

    Some cleanup of code.

--- pymatgen/analysis/synthesis.py
+++ pymatgen/analysis/synthesis.py
@@ -13,43 +13,58 @@ import numpy as np
 from anytree import Node, RenderTree
 
 
-def is_element_or_binary(entry):
-    return len(entry.composition) <= 2
-
-
 __author__ = "Alex Dunn, Anubhav Jain, Shyue Ping Ong"
 __copyright__ = "Copyright 2012, The Materials Project"
 
 
+class PDSynthesisTree:
 
-class SynthesisPathAnalyzer:
+    """
+    This class generates a synthesis tree using a phase diagram analysis.
+    Starting from a given composition, it iteratively breaks down likely
+    synthesis paths up to a certain number of elements, e.g., binaries or
+    elements.
+    """
 
-    def __init__(self, entries):
+    def __init__(self, entries, max_nelement=2):
+        """
+
+        :param entries ([ComputedEntry]): The computed entries from which to
+            perform the phase diagram analysis.
+        :param nelement_reactant (float): This sets a limit as to how many
+            elements each reactant should have before the analysis is stopped.
+            Default is 2 for binaries. Set to 1 for elements.
+        """
         self.pd = PhaseDiagram(entries)
         self.stable_entries = self.pd.stable_entries
+        self.max_nlement = max_nelement
 
-    def get_reaction_tree(self, target, to_remove=None, parent=None):
+    def get_reaction_tree(self, target):
         # Recursive algo to get all reactions
         target = Composition(target)
-        if to_remove is None:
-            to_remove = {}
-        if parent is None:
-            parent = Node(target.reduced_formula,
-                          decomp={PDEntry(target, 0): 1})
-        to_remove = set(to_remove)
-        to_remove.add(target.reduced_formula)
-        new_stable = [e for e in self.stable_entries if
-                      e.composition.reduced_formula not in to_remove]
-        pd = PhaseDiagram(new_stable)
-        decomp = pd.get_decomposition(target)
-        rx_str = " + ".join(
-            sorted([e.composition.reduced_formula for e in decomp.keys()]))
-        child = Node(rx_str, parent, decomp=decomp)
-        for e in decomp.keys():
-            if not is_element_or_binary(e):
-                to_remove.add(e.composition.reduced_formula)
-                self.get_reaction_tree(target, to_remove, parent=child)
-        return parent
+
+        def _get_tree(parent, to_remove):
+            to_remove = set(to_remove)
+            to_remove.add(target.reduced_formula)
+            new_stable = [e for e in self.stable_entries if
+                          e.composition.reduced_formula not in to_remove]
+            pd = PhaseDiagram(new_stable)
+            decomp = pd.get_decomposition(target)
+            rx_str = " + ".join(
+                sorted([e.composition.reduced_formula for e in decomp.keys()]))
+            child = Node(rx_str, parent, decomp=decomp,
+                         avg_nelements=np.mean([len(e.composition) for e in decomp.keys()]))
+            for e in decomp.keys():
+                if not len(e.composition) <= self.max_nlement:
+                    to_remove.add(e.composition.reduced_formula)
+                    _get_tree(child, to_remove)
+            return parent
+
+        t = Node(target.reduced_formula,
+                 decomp={PDEntry(target, 0): 1},
+                 avg_nelements=len(target))
+
+        return _get_tree(t, set())
 
 
 def print_rxn_tree(rxn_tree, target, balanced_rxn_str=False):
@@ -60,12 +75,13 @@ def print_rxn_tree(rxn_tree, target, balanced_rxn_str=False):
             name = str(rxn).split("-")[0]
         else:
             name = node.name
-        print("%s%s" % (pre, name))
+        print("%s%s (avg_nelements = %.2f)" % (pre, name, node.avg_nelements))
 
 
 from pymatgen.util.testing import PymatgenTest
 
-class SynthesisPathAnalyzerTest(PymatgenTest):
+
+class PDSynthesisTreeTest(PymatgenTest):
 
     @classmethod
     def setUpClass(cls):
@@ -74,13 +90,14 @@ class SynthesisPathAnalyzerTest(PymatgenTest):
 
     def test_get_reactions(self):
         target = "LiFeO2"
-        a = SynthesisPathAnalyzer(self.lfo_entries)
+        a = PDSynthesisTree(self.lfo_entries)
         rxn_tree = a.get_reaction_tree(target)
         print_rxn_tree(rxn_tree, target)
-        # Balancing rxn is costly due to inefficient implementation for now, and creates lots of
-        # visual noise. We do this for this small system to illustrate what it is for.
-        # In most cases, we just want to know the phases participating in the reaction.
+        # Balancing rxn is costly due to inefficient implementation for now,
+        # and creates lots of visual noise. We do this for this small system to
+        # illustrate what it is for. In most cases, we just want to know the phases participating in the reaction.
         print_rxn_tree(rxn_tree, target, balanced_rxn_str=True)
+
 
 if __name__ == "__main__":
     import unittest
